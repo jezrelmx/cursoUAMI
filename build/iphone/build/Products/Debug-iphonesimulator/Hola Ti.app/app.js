22c3dee6daf42d1cf03446629e67fd6afcb030ac
Efren Valdez <jezrelmx1304@gmail.com>
2016-08-10 20:15:33 -0500

    maquetado inicial de alarma

--- build/iphone/build/Products/Debug-iphonesimulator/Hola Ti.app/app.js
+++ build/iphone/build/Products/Debug-iphonesimulator/Hola Ti.app/app.js
@@ -1,17 +1,91 @@
-var arreglo_colores = ['#BF360C', '#D84315', '#E64A19', '#F4511E', '#FF5722', '#FF7043', '#FF8A65', '#FFAB91', '#FFCCBC', '#FBE9E7'];
-var arreglo_medidas = ['90%', '80%', '70%', '60%', '50%', '40%', '30%', '20%', '10%'];
 var ventana = Titanium.UI.createWindow({
-	backgroundColor : arreglo_colores[0]
+	backgroundImage : 'fondo_chica.png',
+	layout : 'vertical'
 });
 
-for (var i = 1; i < arreglo_colores.length; i++) {
-	var vista = Titanium.UI.createView({
-		backgroundColor : arreglo_colores[i],
-		width : arreglo_medidas[i - 1],
-		height : arreglo_medidas[i - 1]
-	});
+/**
+ * ------- VISTA SUPERIOR -------
+ */
+var view_sup = Titanium.UI.createView({
+	backgroundColor : 'red',
+	opacity : 0.5,
+	width : Titanium.UI.FILL,
+	height : '60%'
+});
 
-	ventana.add(vista);
-};
+var view_mes_hora = Titanium.UI.createView({
+	backgroundColor : 'pink',
+	opacity : 0.5,
+	width : Titanium.UI.SIZE,
+	height : Titanium.UI.SIZE,
+	top: '15%',
+	layout : 'vertical'
+});
 
+var lb_mes = Titanium.UI.createLabel({
+	text : 'Ago',
+	color : '#FFFFFF',
+	font : {
+		fontSize : 24
+	}
+});
+
+var lb_hora = Titanium.UI.createLabel({
+	text : '21:30',
+	color : '#FFFFFF',
+	font : {
+		fontSize : 40
+	}
+});
+
+view_mes_hora.add(lb_mes);
+view_mes_hora.add(lb_hora);
+
+/**
+ * ------- VISTA INFERIOR -------
+ */
+var view_inf = Titanium.UI.createView({
+	backgroundColor : 'blue',
+	opacity : 0.5,
+	width : Titanium.UI.FILL,
+	height : '40%',
+	layout: 'vertical'
+});
+
+var view_dias = Titanium.UI.createView({
+	backgroundColor : 'purple',
+	opacity : 0.5,
+	width : Titanium.UI.FILL,
+	height : '25%'
+});
+
+var view_alarma_1 = Titanium.UI.createView({
+	backgroundColor : 'orange',
+	opacity : 0.5,
+	width : Titanium.UI.FILL,
+	height : '25%'
+});
+
+var view_alarma_2 = Titanium.UI.createView({
+	backgroundColor : 'yellow',
+	opacity : 0.5,
+	width : Titanium.UI.FILL,
+	height : '25%'
+});
+
+var view_alarma_3 = Titanium.UI.createView({
+	backgroundColor : 'cyan',
+	opacity : 0.5,
+	width : Titanium.UI.FILL,
+	height : '25%'
+});
+
+view_sup.add(view_mes_hora);
+ventana.add(view_sup);
+
+view_inf.add(view_dias);
+view_inf.add(view_alarma_1);
+view_inf.add(view_alarma_2);
+view_inf.add(view_alarma_3);
+ventana.add(view_inf);
 ventana.open();
